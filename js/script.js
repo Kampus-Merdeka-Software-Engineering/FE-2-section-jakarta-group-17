3e64705c242d50ab244fa7403f68b61e2cfb730f
RistyFaren <21082010019@student.upnjatim.ac.id>
2023-12-11 03:29:20 +0700

    FE fix

--- js/script.js
+++ js/script.js
@@ -9,7 +9,25 @@ navbarNav.classList.toggle('active');
 //klik diluar sidebar untuk menghilangkan nav (EVENT LISTENER)
 const  humberger=document.querySelector('#hamburger-menu')
 document.addEventListener('click', function(e){
-    if(!humberger.contains(e.targer)&& !navbarNav.contains(e.target)){
+    if(!humberger.contains(e.target)&& !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
     }
 })
+
+// klik product lalu keluar preview product
+let previewContainer = document.querySelector('.product-preview');
+let previewBox = previewContainer.querySelectorAll('preview ');
+document.querySelectorAll('.product-container .product').forEach(product =>{product.onclick = () => {
+    previewContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+previewBox.forEach(preview =>{
+    let target = preview.getAttribute ('data-target');
+    if(name== target){
+        preview.classList.add('active');
+    }
+});
+};
+});
+
+
+
